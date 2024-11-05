7fe8083a8c6bd6ba754e29edb39d2467b8f2ff38
nixoletas <nicholetas@gmail.com>
2024-11-05 08:09:50 -0400

    inclui dicas para uso do fontawesome e rawline

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,9 @@
 	<meta charset="UTF-8">
 	<title>Arranchamento</title>
 	<link rel="icon" type="image/png" href="assets/img/logo_arranchamento.png">
+	<!-- Selecione o fontawesome da web, pois o local pode não funcionar não está funcionando -->
 	<link rel="stylesheet" href="http://10.56.19.152/assets/font-awesome/css/all.min.css" media="print" onload="this.media='all'"/>
+	<!-- Selecione o Rawline da web, pois o local pode não funcionar -->
 	<link rel="stylesheet" href="http://10.56.19.152/assets/rawline/rawline.css" media="print" onload="this.media='all'" />
 	<link rel='stylesheet' href='/assets/css/bootstrap.css'>
 	<link rel="stylesheet" href="/assets/css/style.css">
--- inc/skeleton.php
+++ inc/skeleton.php
@@ -4,7 +4,6 @@
 				<div class="row">
 					<img style="width:80%;padding-bottom: 30px;" src="assets/img/logo.png" alt="Logo" class="img-responsive center" />
 					<div class="list-group">
-						<!-- <a class="list-group-item" href="#" data-toggle="modal" data-target="#login-modal"><i class="fa fa-user"></i> Login</a> -->
 						<a class="list-group-item" href="logout.php"><i class="fa fa-sign-out"></i> Logout</a>
 					</div>
 				</div>
